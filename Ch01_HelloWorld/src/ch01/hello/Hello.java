fc1d5d9cc2ed5d3b4bd59b09985d805090410241
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:31:46 +0900

    Hello, Java 출력 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -3,6 +3,7 @@ package ch01.hello;
 public class Hello {
 	// 프로그램에서 프로그램 시작부분을 main으로 설정
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 
+		System.out.println("Hello World");
+		System.out.println("Hello, Java");
 	}
 }
