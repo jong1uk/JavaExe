e4a4e9377c56367d880007470e6cbe0a330d1eed
jong1uk <jju3560@gmail.com>
2024-01-25 10:52:17 +0900

    Test

---
+++ Ch02_Variable/src/ch02/operator04/CompOperator.java
@@ -0,0 +1,21 @@
+package ch02.operator04;
+
+public class CompOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num0 = 100;
+		int num1 = 10;
+		int num2 = 100;
+		
+		boolean bVal = num0 > num1;
+		System.out.println(bVal);
+		bVal  = num0 < num1;
+		System.out.println(bVal);
+		bVal = num0 == num2;
+		System.out.println(bVal);
+		bVal = num0 >= num2;
+		System.out.println(bVal);
+	}
+
+}
---
+++ Ch02_Variable/src/ch02/transtype05/TransDataType.java
@@ -0,0 +1,10 @@
+package ch02.transtype05;
+
+public class TransDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
